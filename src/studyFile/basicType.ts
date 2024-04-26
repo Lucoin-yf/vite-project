2012527ac1a44960120268db6b83652f4613f0ab
Lucoin <148014505+Lucoin-yf@users.noreply.github.com>
2024-04-26 15:00:09 +0000

    studyFileForType1

---
+++ src/studyFile/basicType.ts
@@ -0,0 +1,17 @@
+const names: string = 'jack'
+
+const age: number = 12
+
+const isTrue: boolean = true
+
+let a: undefined
+
+const b: null = null
+
+const user: object = {}
+
+const big: bigint = 100n //加n才表示bigint类型
+
+const sym: symbol = Symbol('key')
+
+export default {} // 为了解决模块化问题，这里导出一个空对象
